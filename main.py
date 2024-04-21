0480b17ea8ea6f32bd10e5432fed41f703fee514
ckmalipio <cyriskenmalipio@gmail.com>
2024-04-21 21:14:22 +0800

    model

---
+++ main.py
@@ -0,0 +1,8 @@
+import flet as ft
+from flet import TextField, Checkbox, ElevatedButton, Text, Row 
+from flet import Column, View, Page, AppBar, ElevatedButton
+from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
+from flet_core.control_event import ControlEvent
+
+def main():
+    
