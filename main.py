198f5877a1c7833a87426412ab313cce2a2700a7
ckmalipio <cyriskenmalipio@gmail.com>
2024-04-21 21:52:40 +0800

    tkinter

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import flet as ft
-from flet import TextField, Checkbox, ElevatedButton, Text, Row 
-from flet import Column, View, Page, AppBar, ElevatedButton
-from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
-from flet_core.control_event import ControlEvent
+import tkinter as tk
 
-def main():
-    
+root = tk.Tk()
+
+root.geometry("1280x720")
+root.title("Vita-Vault")
+
+label =tk.Label(root, text='Hello World!', font=('Tahoma', 18))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, height=3, font=('Tahoma', 16))
+textbox.pack(padx=10)
+
+button = tk.Button(root, text='Click Me!', font=('Tahoma', 18))
+button.pack()
+
+root.mainloop()
