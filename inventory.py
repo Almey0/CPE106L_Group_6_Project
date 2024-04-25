cf239d3eb5194b9b5dea0b7a9cb404279196bd56
ckmalipio <cyriskenmalipio@gmail.com>
2024-04-25 20:03:09 +0800

    inventory ui

--- inventory.py
+++ inventory.py
@@ -6,3 +6,13 @@ def inventory_window():
     inventory.config(height=500, width=500)
     inventory.geometry('500x500')
     inventory.resizable(False, False)
+    
+    frame = Frame(inventory)
+    
+    label_title = Label(frame, 
+                        text='Inventory', 
+                        font=('Tahoma', 40))
+    
+    frame.pack()
+    
+    frame.mainloop()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import sqlite3
 from inventory import *
 from supplier import *
 from transaction import *
@@ -13,15 +12,37 @@ root.title("Login")
 frame = Frame(root)
 frame.pack(padx=50, pady=10)
 
-label_title = Label(frame, text='Vita-Vault', font=('Arial', 40))
-label_title.grid(row=0, column=0, pady=20, columnspan=2)
-button_inventory = Button(frame, text='Inventory', font=('Tahoma', 15), height=2, width=10, command=inventory_window)
-button_inventory.grid(row=1, column=0, padx=10, pady=10)
-button_transaction = Button(frame, text='Transaction', font=('Tahoma', 15), height=2, width=10, command=transaction_window)
+label_title = Label(frame, 
+                    text='Vita-Vault', 
+                    font=('Arial', 40))
+label_title.grid(row=0, column=0, 
+                 pady=20, 
+                 columnspan=2)
+
+button_inventory = Button(frame, 
+                          text='Inventory', 
+                          font=('Tahoma', 15), 
+                          height=2, width=10, 
+                          command=inventory_window)
+button_inventory.grid(row=1, column=0, 
+                      padx=10, pady=10)
+button_transaction = Button(frame, 
+                            text='Transaction', 
+                            font=('Tahoma', 15), 
+                            height=2, width=10, 
+                            command=transaction_window)
 button_transaction.grid(row=1, column=1)
-button_supplier = Button(frame, text='Supplier', font=('Tahoma', 15), height=2, width=10, command=supplier_window)
+button_supplier = Button(frame, 
+                         text='Supplier', 
+                         font=('Tahoma', 15), 
+                         height=2, width=10, 
+                         command=supplier_window)
 button_supplier.grid(row=2, column=0)
-button_exit = Button(frame, text='Exit', font=('Tahoma', 15), height=2, width=10, command=root.destroy)
+button_exit = Button(frame, 
+                     text='Exit', 
+                     font=('Tahoma', 15), 
+                     height=2, width=10, 
+                     command=root.destroy)
 button_exit.grid(row=2, column=1)
 
 root.mainloop()
