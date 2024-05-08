15a673b50715bf280215417498e0d63ea86a0308
ckmalipio <cyriskenmalipio@gmail.com>
2024-05-08 15:22:03 +0800

    fix

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ from supplier import *
 from transaction import *
 
 root = Tk()
-root.geometry('500x350')
-root.config(height=400, width=350)
+root.geometry('300x200')
+root.config(width=300, height=200)
 root.resizable(False, False)
 root.title("Login")
 
 frame = Frame(root)
 
 label_title = Label(frame, text='Vita-Vault', font=('', 40))
-label_title.grid(row=0, column=0, pady=20, columnspan=2)
+label_title.grid(row=0, column=0, columnspan=2)
 
-button_inventory = Button(frame, text='Inventory', font=('', 20), height=2, width=10, command=inventory_window)
-button_inventory.grid(row=1, column=0, padx=10, pady=10)
+button_inventory = Button(frame, text='Inventory',  command=inventory_window)
+button_inventory.grid(row=1, column=0, padx=(0, 10), pady=10)
 
-button_transaction = Button(frame, text='Transaction', font=('', 20), height=2, width=10, command=transaction_window)
+button_transaction = Button(frame, text='Transaction', command=transaction_window)
 button_transaction.grid(row=1, column=1)
 
-button_supplier = Button(frame, text='Supplier', font=('', 20), height=2, width=10, command=supplier_window)
+button_supplier = Button(frame, text='Supplier', command=supplier_window)
 button_supplier.grid(row=2, column=0)
 
-button_exit = Button(frame, text='Exit', font=('', 20), height=2, width=10, command=root.destroy)
+button_exit = Button(frame, text='Exit', command=root.destroy)
 button_exit.grid(row=2, column=1)
 
-frame.pack(padx=50, pady=10)
+frame.pack(pady=10)
 
 root.mainloop()
